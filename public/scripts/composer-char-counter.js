da00fd9c77af1716cb5e2991e63bbc5ba2720b91
Maxwell Veer <maxaveer@gmail.com>
2022-03-31 04:13:02 +0000

    css stylesheets for the template of our individual tweets, lots of styling adjustments as well(the hover buttons are not yet complete)

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,12 +4,13 @@ $(document).ready(function() {
   // --- our code goes here ---
   console.log('we are working now')
   
-  const maximumLength = 140;
+  const maximumLength = 144;
   //point/target the element containing the counter.
   $('#tweet-text').on('input', (event) => {
     //console.log(this)
     const output = event.target.value
     let charRemaining = maximumLength - output.length;
+    
     //console.log(charRemaining, this)
     if (charRemaining <= 0) {
       $('.counter').css("color", "rgb(247 34 3)")
