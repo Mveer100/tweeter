e4f30e38f54d7ef8f613297213944e585dc08c1c
Maxwell Veer <maxaveer@gmail.com>
2022-04-04 05:01:48 +0000

    almost lost it all last push but we managed to get it fixed, finalized all pieces of the project

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,27 +7,35 @@
 */
 //const $tweet = $("<article class='tweet'> Hello world</article>");
 $(document).ready(function () {
+  $('div#nothingPost').hide();
   //take the tweet object and use its data to flil in the tweet template from tweet template
 
   //this requires the following data to be filled:
 
   // --username,  user avatars,   user handle. text body (content:text) and creation time
 
+  const escape = function (str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
 
   const createTweetElement = (tweetObj) => {
-    const $tweet = $(
+
+    const tweet = 
       `<article class="tweet">
         <header class="tweetsHeader">
           <div>
             <div class="posterPic">
-              <img src="/images/profile-hex.png">
+              <img src="/images/profile-hex.png"><strong>
               ${tweetObj.user.name}
+              </strong>
           </div>
-          <aside>${tweetObj.user.handle}</aside>
+          <aside><strong>${tweetObj.user.handle} </strong></aside>
           </div>
         </header>
         <article class="tweetbody">
-          ${tweetObj.content.text}
+          ${escape(tweetObj.content.text)}
         </article>
         <footer class='tweetsFooter'>
           <span>
@@ -39,9 +47,8 @@ $(document).ready(function () {
           <i class="fa-solid fa-lungs"></i>       
           </div>
         </footer>
-      </article>`
-    );
-    return $tweet;
+      </article>`;
+    return tweet;
   }
     //  $('main').prepend($tweet); 
 
@@ -54,20 +61,31 @@ $(document).ready(function () {
       $('.tweeterPosts').prepend(createTweetElement(element))
     });
   }
-
+  const loadTweet = function () {
+    $.ajax('/tweets/', { method: 'GET' })
+      .then(function (tweets) {
+        console.log("we made it dude", tweets);
+        renderTweets(tweets);
+      });
+  }
+  loadTweet();
   //renderTweets(data);
   // use jQuery to create a listener for our tweet submission
   //create a post request to send form data
   $("#tweetform").submit(function (event) {
-    alert("Handler for .submit() called.");
+    //alert("Handler for .submit() called.");
     event.preventDefault();
-    const logging = $("#tweet-text").serialize()
-    //we weill render a message on the page if the characters remaining is less than 0
-    //render a message to the user if the form was submitted empty
-    if (logging.length > 149) {
-      alert('You have typed in too many characters')
+    const logging = $("#tweetform").serialize();
+    //we weill render a message in an alert if the characters remaining is less than 0
+    console.log(logging.length)
+    //render a message to the user if the form was submitted empty. 145 used below as it is our limit 140 + 5 to account for serializer.
+    if (logging.length > 145) {
+      event.stopPropogation()
+    } if (logging.length <= 5) {
+      $('#nothingPost').slideToggle('slow');
       event.stopPropogation()
     }
+
     console.log(logging.length)// take away 5 from logging.length as it is serialized as: text=
     const url = '/tweets/';
     const settings = {
@@ -78,17 +96,9 @@ $(document).ready(function () {
     $.ajax(url, settings)
       .then(function (apiResponse) {
         console.log("this ajaxcallworks")
+        loadTweet();
       });
-    //
-  });
-
+      //
+    }); 
   //use jquery to make an ajax get request to tweets, this data is going to be rendered once this is completed using our previously declared function.(l.50)
-  const loadTweet = function () {
-    $.ajax('/tweets/', { method: 'GET' })
-      .then(function (tweets) {
-        console.log("we made it dude", tweets);
-        renderTweets(tweets);
-      });
-  }
-  loadTweet();
 });
